c5e84abf2d3af005429aa84eb33636105fa010d4
Mikko Suokas <121874134+Hienomekaanikko@users.noreply.github.com>
2024-02-16 10:09:26 +0200

    Create fridge.py
    Basic fridge functionalities added: 
    
    - available space
    - updating of the space when unloading the shopping bags
    - updating the current supply of the fridgerator based on what has been bought.

---
+++ fridge.py
@@ -0,0 +1,26 @@
+class Fridge:
+    def __init__(self):
+        self.supply = {}
+        self.space = 150 #liters
+        self.shelfs = 5 #shelfs
+        self.temperature = 6 #celsius
+
+    #Method to update bought groceries into the current supply of fridgerator
+    def input_groceries(self):
+
+        #Start unloading the shoppingbags
+        not_finished = True 
+        while not_finished:
+            purchase = input("Purchase: (type 'finished' when done.)")
+            if purchase == "finished":
+                not_finished = False
+            else:
+                amount = int(input("Amount in liters: "))
+                self.space -= amount
+              
+                #Check if space available in the fridgerator
+                if self.space < 0:
+                    return f"No more space in the fridge left."
+                self.supply[purchase] = self.supply.get(purchase, 0) + amount
+        current_supply = self.supply
+        return current_supply
