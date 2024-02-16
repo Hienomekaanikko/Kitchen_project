19fa9422111a73bb4eb61f3113db5c0182278c9e
Mikko Suokas <121874134+Hienomekaanikko@users.noreply.github.com>
2024-02-16 19:45:48 +0200

    Create items.py
    Most typical foods and groceries so that these can be classified as liquids or solids.

---
+++ items.py
@@ -0,0 +1,8 @@
+class Liquid:
+    meats = ["sausage", "minced_meat", "chicken", "beef", "lamb"]
+    drinks = ["still_water", "sparkling_water", "juice", "beer", "milk"]
+
+class Solid:
+    vegetables = ["salad", "cucumber", "tomato"]
+    fruits = ["orange", "kiwi", "apple"]
+    roots = ["onion", "potato"]
