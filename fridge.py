2f387597992d949661926af8f11e4eac3bcca85b
mikkosuokas <mikesuokas@gmail.com>
2024-02-16 10:29:01 +0200

    Impoved readability

--- fridge.py
+++ fridge.py
@@ -1,15 +1,15 @@
 class Fridge:
     def __init__(self):
         self.supply = {}
-        self.space = 150 #liters
-        self.shelfs = 5 #shelfs
-        self.temperature = 6 #celsius
+        self.space = 150  # liters
+        self.shelf = 5  # shelf's
+        self.temperature = 6  # celsius
 
-    #Update bought groceries into the current supply of fridgerator
+    # Update bought groceries into the current supply of refrigerator
     def input_groceries(self):
 
-        #Start unloading the shoppingbags
-        not_finished = True 
+        # Start unloading the shopping bags
+        not_finished = True
         while not_finished:
             purchase = input("Purchase: (type 'finished' when done.)")
             if purchase == "finished":
@@ -17,8 +17,8 @@ class Fridge:
             else:
                 amount = int(input("Amount in liters: "))
                 self.space -= amount
-              
-                #Check if space available in the fridgerator
+
+                # Check if space available in the refrigerator
                 if self.space < 0:
                     return f"No more space in the fridge left."
                 self.supply[purchase] = self.supply.get(purchase, 0) + amount
