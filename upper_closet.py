13b6586241452517a829a0afecb13db06891f699
mikkosuokas <mikesuokas@gmail.com>
2024-02-19 08:03:24 +0200

    add items to the upper closet

--- upper_closet.py
+++ upper_closet.py
@@ -4,3 +4,10 @@ class UpCloset:
         self.food_plate = 10
         self.shallow_plate = 10
         self.bread_plate = 10
+
+    #choosing appropriate dish
+    def take_dish(self):
+        choice = input("Which dish would you need? glass/foodplate/shallowplate/breadplate?")
+
+    #how many dishes available/how many in dishwasher
+    def check_inventory(self):
