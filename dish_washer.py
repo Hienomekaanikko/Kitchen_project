9df2bfdb1b8017d22d2103f977804be4e0331e49
mikkosuokas <mikesuokas@gmail.com>
2024-02-19 08:19:06 +0200

    function to wash the dishes

--- dish_washer.py
+++ dish_washer.py
@@ -1,3 +1,5 @@
+import time
+
 class Dishwasher:
     # How much room for each item
     def __init__(self):
@@ -14,3 +16,14 @@ class Dishwasher:
             setattr(self, f"{item}_places", getattr(self, f"{item}_places") - 1)
         else:
             print("Invalid dish choice.")
+
+    def run_dishwasher(self):
+        running = True
+        while running:
+            print("BrRrRrRrRrRrR...")
+            time.sleep(10)
+            print("Beep, Boop. Dishes are ready and clean!")
+            running = False
+
+    def empty_dishwasher(self):
+
