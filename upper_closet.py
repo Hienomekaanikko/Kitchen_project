50100c9b6ae8dba7401f8d3cedb65ab9cd89bfa5
mikkosuokas <mikesuokas@gmail.com>
2024-02-19 08:12:48 +0200

    way to take items from the upper closet

--- upper_closet.py
+++ upper_closet.py
@@ -1,13 +1,20 @@
 class UpCloset:
     def __init__(self):
         self.glass = 10
-        self.food_plate = 10
-        self.shallow_plate = 10
-        self.bread_plate = 10
+        self.foodplate = 10
+        self.shallowplate = 10
+        self.breadplate = 10
 
     #choosing appropriate dish
     def take_dish(self):
-        choice = input("Which dish would you need? glass/foodplate/shallowplate/breadplate?")
+        choosing = True
+        while choosing:
+            choice = input("Which dish would you need? glass/foodplate/shallowplate/breadplate?")
+            if hasattr(self, f"{choice}"):
+                setattr(self, f"{choice}", getattr(self, f"{choice}") -1)
+                choosing = False
+            else:
+                print("Don't have this one")
 
     #how many dishes available/how many in dishwasher
     def check_inventory(self):
