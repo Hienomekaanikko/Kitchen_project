70e7d668a6ae7ea835033bcb5ec0ad8e77ad5316
mikkosuokas <mikesuokas@gmail.com>
2024-02-23 14:09:19 +0200

    Microwave updates

--- microwave.py
+++ microwave.py
@@ -1,13 +1,32 @@
+import time
+
 class Microwave:
     def __init__(self):
         pass
 
     def set_timer(self):
+        timer = int(input("Set the timer in seconds: "))
+        running = True
+        while running:
+            print("*microwave sound*...")
+            for i in range(timer, 0, -1):
+                time.sleep(1)
+                print(f"\r{i} seconds left", end="", flush=True)
+            print("\r             ", end="", flush=True)
+            print("\rBeep, Beep, beep! Microwaving finished!")
+            running = False
 
     def set_power(self):
+        pass
 
     def input_food(self):
+        pass
 
     def take_food(self):
+        pass
 
     def beep_sound(self):
+        pass
+
+microwave = Microwave()
+microwave.set_timer()
