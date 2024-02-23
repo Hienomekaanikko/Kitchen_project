e20c9ffedd23139f84c617da5404d3f141da9d58
mikkosuokas <mikesuokas@gmail.com>
2024-02-23 14:04:22 +0200

    microwave updates

--- microwave.py
+++ microwave.py
@@ -1,3 +1,13 @@
 class Microwave:
     def __init__(self):
-        self.timer
+        pass
+
+    def set_timer(self):
+
+    def set_power(self):
+
+    def input_food(self):
+
+    def take_food(self):
+
+    def beep_sound(self):
