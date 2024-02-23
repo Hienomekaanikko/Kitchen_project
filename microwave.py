9ddd8d2f21375f3936b326c5631278e126105bb7
mikkosuokas <mikesuokas@gmail.com>
2024-02-23 14:40:41 +0200

    Microwave updates

--- microwave.py
+++ microwave.py
@@ -5,8 +5,9 @@ class Microwave:
         self.space = 10 #liters
         self.dirt = 10 #in scale from 10 to being dirtiest to 1 being cleanest
 
-    def set_timer(self):
+    def run_microwave(self):
         timer = int(input("Set the timer in seconds: "))
+        power = int(input("Set the power in watts: "))
         running = True
         while running:
             print("*microwave sound*...")
@@ -16,9 +17,8 @@ class Microwave:
             print("\r             ", end="", flush=True)
             print("\rBeep, Beep, beep! Microwaving finished!")
             running = False
-
-    def set_power(self):
-        pass
+        print(f"Microwave ran for {timer} seconds with {power} watts.")
+        return timer, power
 
     def input_food(self):
         pass
